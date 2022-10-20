56500c30f67ff6e7488aaa0757b92f4f9892708f
MD ASIF KABIR EMON <mdasifkabiremon@MDs-MacBook-Air.local>
2022-10-20 15:14:39 +0800

    second commit

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const port = process.env.PORT || 4000;
 app.use(cors());
 
 const categories = require("./data/categories.json");
+const news = require("./data/news.json");
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
@@ -13,6 +14,22 @@ app.get("/", (req, res) => {
 
 app.get("/new-categories", (req, res) => {
   res.send(categories);
+});
+
+app.get("/news/:id", (req, res) => {
+  const id = req.params.id;
+  const selectedNews = news.find((n) => n._id === id);
+  res.send(selectedNews);
+});
+
+app.get("/category/:id", (req, res) => {
+  const id = req.params.id;
+  if (id === "08") {
+    res.send(news);
+  } else {
+    const categoryNews = news.filter((n) => n.category_id === id);
+    res.send(categoryNews);
+  }
 });
 
 app.listen(port, () => {
